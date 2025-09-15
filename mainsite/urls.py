610ef513666ceed34693d6f5397a1b55cfdbfe3a
rockefelm <rockefelm@gmail.com>
2025-09-14 22:23:19 -0400

    added the main endpoint to just redirect to the forum endpoint for the time being.

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
+    path('', lambda request: redirect('forum:thread-list', permanent=False)),  # ← Root goes to forum
 ]
