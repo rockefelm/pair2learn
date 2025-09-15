b27480ff2b070f286863ffca00863ac7a54c23de
rockefelm <rockefelm@gmail.com>
2025-09-14 20:59:40 -0400

    added an initial basic forum to the site.

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['pair2learn.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'forum',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
